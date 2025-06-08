b8cb85cf03b2549f32e4e61a066993e98ef963f2
Covenant Ekojoe <150958017+Ekojoecovenant@users.noreply.github.com>
2025-06-08 14:01:01 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,6 +13,10 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.use("/ping", (req, res) => {
+  console.log(req.url, ", Pong!");
+  res.json({ message: "Pong!" });
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/products", productRoutes);
